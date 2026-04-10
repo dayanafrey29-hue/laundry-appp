a631b0161156d5ac06ffadd2aa66d582efac1b41
dayanafrey29-hue <dayanafrey29@gmail.com>
2026-04-10 21:10:45 +0000

    Changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect, useRef } from "react";
-import { supabase } from './supabaseClient'; // ПІДКЛЮЧАЄМО SUPABASE
+import { supabase } from '../supabaseClient'; // ПІДКЛЮЧАЄМО SUPABASE
 
 const SETTINGS_PASSWORD = "2026";
 
